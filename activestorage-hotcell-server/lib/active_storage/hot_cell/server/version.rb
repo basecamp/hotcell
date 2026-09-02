543e6b550e7e3ab737342d1c1410354e8fdb24c1
Mike Dalessio <mike@37signals.com>
2026-09-02 13:01:09 -0400

    version bump to 0.3.2.dev

--- activestorage-hotcell-server/lib/active_storage/hot_cell/server/version.rb
+++ activestorage-hotcell-server/lib/active_storage/hot_cell/server/version.rb
@@ -3,7 +3,7 @@
 module ActiveStorage
   module HotCell
     module Server
-      VERSION = "0.3.1"
+      VERSION = "0.3.2.dev"
     end
   end
 end
--- hotcell-server/lib/hot_cell/server/version.rb
+++ hotcell-server/lib/hot_cell/server/version.rb
@@ -2,6 +2,6 @@
 
 module HotCell
   module Server
-    VERSION = "0.3.1"
+    VERSION = "0.3.2.dev"
   end
 end
